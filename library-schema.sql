c0efc36d3137d53d95db7179b2716a94f37a70e1
Thanasisrem <thanasisrem@hotmail.com>
2023-05-17 01:16:55 +0300

    Created Tables: student_professor, borrower_card

--- library-schema.sql
+++ library-schema.sql
@@ -16,7 +16,7 @@ CREATE SCHEMA library; USE
     -- Table structure for table 'users'
     --
 
-CREATE TABLE users(
+CREATE TABLE user(
     user_id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
     username VARCHAR(45) NOT NULL,
     full_name VARCHAR(45) NOT NULL,
@@ -29,22 +29,22 @@ CREATE TABLE users(
 -- Table structure for table 'users_phone_number'
 --
 
-CREATE TABLE users_phone_number(
+CREATE TABLE user_phone_number(
     phone_number VARCHAR(15) NOT NULL,
     user_id SMALLINT UNSIGNED NOT NULL,
     PRIMARY KEY(phone_number, user_id),
-    CONSTRAINT fk_users_phone_number_users FOREIGN KEY(user_id) REFERENCES users(user_id) ON DELETE CASCADE ON UPDATE CASCADE
+    CONSTRAINT fk_user_phone_number_user FOREIGN KEY(user_id) REFERENCES user(user_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 --
 -- Table structure for table 'users_email'
 --
 
-CREATE TABLE users_email(
+CREATE TABLE user_email(
     email VARCHAR(45) NOT NULL,
     user_id SMALLINT UNSIGNED NOT NULL,
     PRIMARY KEY(email, user_id),
-    CONSTRAINT fk_users_email_users FOREIGN KEY(user_id) REFERENCES users(user_id) ON DELETE CASCADE ON UPDATE CASCADE
+    CONSTRAINT fk_user_email_user FOREIGN KEY(user_id) REFERENCES user(user_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 --
@@ -54,7 +54,7 @@ CREATE TABLE users_email(
 CREATE TABLE administrator(
     admin_user_id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
     PRIMARY KEY(admin_user_id),
-    CONSTRAINT fk_administrator_users FOREIGN KEY(admin_user_id) REFERENCES users(user_id) ON DELETE CASCADE ON UPDATE CASCADE
+    CONSTRAINT fk_administrator_user FOREIGN KEY(admin_user_id) REFERENCES user(user_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 --
@@ -77,12 +77,42 @@ CREATE TABLE school_unit(
 -- Table structure for table 'operator'
 --
 
-CREATE TABLE operators(
+CREATE TABLE operator(
     operator_user_id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
     admin_user_id SMALLINT UNSIGNED NOT NULL,
     school_id SMALLINT UNSIGNED NOT NULL,
     PRIMARY KEY(operator_user_id),
-    CONSTRAINT fk_operators_users FOREIGN KEY(operator_user_id) REFERENCES users(user_id) ON DELETE RESTRICT ON UPDATE CASCADE,
-    CONSTRAINT fk_operators_administrator FOREIGN KEY(admin_user_id) REFERENCES administrator(admin_user_id) ON DELETE RESTRICT ON UPDATE CASCADE,
-    CONSTRAINT fk_operators_school_unit FOREIGN KEY(school_id) REFERENCES school_unit(school_id) ON DELETE RESTRICT ON UPDATE CASCADE
+    CONSTRAINT fk_operator_user FOREIGN KEY(operator_user_id) REFERENCES user(user_id) ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT fk_operator_administrator FOREIGN KEY(admin_user_id) REFERENCES administrator(admin_user_id) ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT fk_operator_school_unit FOREIGN KEY(school_id) REFERENCES school_unit(school_id) ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+--
+-- Table structure for table 'student_professor'
+--
+
+CREATE TABLE student_professor(
+	stud_prof_user_id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+	role VARCHAR(10) NOT NULL,
+	active_status VARCHAR(15) NOT NULL,
+	approval_status VARCHAR(15) NOT NULL,
+	operator_user_id SMALLINT UNSIGNED NOT NULL, 
+	PRIMARY KEY(stud_prof_user_id),
+	CONSTRAINT fk_student_professor_user FOREIGN KEY(stud_prof_user_id) REFERENCES user(user_id) ON DELETE CASCADE ON UPDATE CASCADE,
+	CONSTRAINT fk_student_professor_operator FOREIGN KEY(operator_user_id) REFERENCES operator(operator_user_id) ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+--
+-- Table structure for tble 'borrower_card'
+--
+
+CREATE TABLE borrower_card(
+	card_id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+	issue_date DATE NOT NULL,
+	expiry_date DATE NOT NULL,
+	stud_prof_user_id SMALLINT UNSIGNED NOT NULL,
+	operator_user_id SMALLINT UNSIGNED NOT NULL,
+	PRIMARY KEY(card_id),
+	CONSTRAINT fk_borrower_card_student_professor FOREIGN KEY(stud_prof_user_id) REFERENCES student_professor(stud_prof_user_id) ON DELETE RESTRICT ON UPDATE CASCADE,
+	CONSTRAINT fk_borrower_card_operator FOREIGN KEY(operator_user_id) REFERENCES operator(operator_user_id) ON DELETE RESTRICT ON UPDATE CASCADE
 );
