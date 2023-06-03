3d1558946c5222bdd33ea17acf6f82aacf5ba1a4
Giannis Litsos <giannislitsos@gmail.com>
2023-06-03 06:20:53 +0300

    Finished queries in the operator mainpage

--- application/controllers/operators.js
+++ application/controllers/operators.js
@@ -50,15 +50,20 @@ exports.handleLogin_operator = (req, res) => {
     });
   };
 
-exports.getBooks = (req, res) => {
-    
+  exports.getBooks = (req, res) => {
     const title = req.query.title;
-    const category = req.query.categroy;
+    const category = req.query.category;
     const author = req.query.author;
-    const copies = req.query.copies;
+    let copies = req.query.copies;
     const operatorId = req.session.operatorId;
     //console.log(operatorId);
-    // console.log(genre);
+    console.log(copies);
+  
+    // Check if copies parameter is "null" and set it to null
+    if (copies === "null") {
+      copies = null;
+    }
+  
     // Query the database with the genre parameter
     pool.getConnection((err, conn) => {
       if (err) {
@@ -66,29 +71,32 @@ exports.getBooks = (req, res) => {
         return res.status(500).send('Internal Server Error');
       }
       conn.promise()
-      .query('CALL list_title_authors(?,?,?,?,?)', [operatorId,title,category,author,copies])
-      .then(([results]) => {
-             // Render the view with both sets of results
-              res.render('books_per_school.ejs', {
-              pageTitle: "Query 3.2.1",
-              results: results,
-            });
-            console.log(results)
-          })
-          .then(() => pool.releaseConnection(conn))
-          .catch(err => console.log(err))
-      })
-        
-    };
+        .query('CALL list_title_authors(?,?,?,?,?)', [operatorId, title, category, author, copies])
+        .then(([results]) => {
+          // Render the view with both sets of results
+          res.render('books_per_school.ejs', {
+            pageTitle: "Query 3.2.1",
+            results: results,
+          });
+          console.log(results);
+        })
+        .then(() => pool.releaseConnection(conn))
+        .catch(err => console.log(err));
+    });
+  };
+  
 
 exports.getLateReturns = (req, res) => {
       const firstname = req.query.firstname;
       const lastname = req.query.lastname;
-      const overdueDays = req.query.overdueDays;
+      let overdueDays = req.query.overdueDays;
       const operatorId = req.session.operatorId;
       //console.log(operatorId);
-      // console.log(genre);
+      console.log(firstname);
       // Query the database with the genre parameter
+      if (overdueDays === "null") {
+        overdueDays = null;
+      }
       pool.getConnection((err, conn) => {
         if (err) {
           console.error(err);
@@ -138,3 +146,67 @@ exports.getAvgRating = (req, res) => {
           })
             
         };
+
+  exports.getOperatorPasswordPage= (req, res, next) => {
+    res.render('operator_password.ejs', {
+    pageTitle: 'Operator change password ',
+    messages: [] 
+        });
+    }
+
+    exports.UpdateOperatorPassword = (req, res, next) => {
+      /* get necessary data sent */
+      const password = req.body.newPassword;
+      const operatorId = req.session.operatorId;
+      pool.getConnection((err, conn) => {
+        var sqlQuery = `UPDATE operator SET password = ? WHERE operator_id = ${operatorId}`;
+        conn.promise().query(sqlQuery, [password])
+          .then(() => {
+            pool.releaseConnection(conn);
+            res.render('operator_mainpage.ejs', {
+              pageTitle: 'Operator Main Page',
+              messages: [] // You can pass any desired messages to display on the admin login page
+            });
+          })
+          .catch(err => {
+            console.log(err);
+            req.flash('messages', { type: 'error', value: "Something went wrong, Executive could not be updated." })
+            res.redirect('/operator_mainpage');
+          })
+      })
+    }
+   
+    exports.getSchoolBooksPage= (req, res, next) => {
+      const operatorId = req.session.operatorId;
+      console.log(operatorId)
+      pool.getConnection((err, conn) => {
+        var sqlQuery = `
+        SELECT b.title, GROUP_CONCAT(DISTINCT a.author) AS author, GROUP_CONCAT(DISTINCT c.thematic_category) AS thematic_category,b.publisher,b.ISBN,b.number_of_pages,b.image,b.language,b.summary,b.key_words, b.available_copies 
+        FROM book b
+        JOIN book_authors a
+        ON b.book_id = a.book_id
+        JOIN book_thematic_categories c
+        ON b.book_id = c.book_id
+        WHERE b.school_id = ${operatorId}
+        GROUP BY b.book_id;`;
+
+        //school_id and operator_id are always the same
+        conn.promise().query(sqlQuery)
+          .then(([results]) => {
+            pool.releaseConnection(conn);
+            res.render('school_books.ejs', {
+              pageTitle: 'School books page ',
+              books:results,
+              messages: [] 
+                  });
+                //console.log(results);
+          })
+          .catch(err => {
+            console.log(err);
+            req.flash('messages', { type: 'error', value: "Something went wrong, Executive could not be updated." })
+            res.redirect('/operator_mainpage');
+          })
+      })
+      }
+  
+
--- application/routes/operators.js
+++ application/routes/operators.js
@@ -8,7 +8,10 @@ router.get('/operator_login',operator_login.getOperatorLoginPage),
 router.post('/operator_mainpage',operator_login.handleLogin_operator),
 router.get('/books_per_school',operator_login.getBooks),
 router.get('/late_returns',operator_login.getLateReturns),
-router.get('/average_rating',operator_login.getAvgRating);
+router.get('/average_rating',operator_login.getAvgRating),
+router.get('/operator_password',operator_login.getOperatorPasswordPage),
+router.post('/operator_password',operator_login.UpdateOperatorPassword),
+router.get('/school_books',operator_login.getSchoolBooksPage);
 
 
 module.exports = router;
